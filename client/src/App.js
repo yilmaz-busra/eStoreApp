c297bdbe65f5b2c8b37ac2065eac2c61ef696d5e
busra <b.busraayilmaz@gmail.com>
2024-02-15 16:02:19 +0300

    Signin&Login&Products-Added&RoutingDone

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+
 import Home from "./Pages/Home";
+
 import Navbar from "./Components/Navbar";
+import Signin from "./Pages/Auth/Signin";
+import Login from "./Pages/Auth/Login";
+import Products from "./Pages/Products/Products";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path={"/"} element={<Home />} />
+        <Route path={"/"} element={<Products />} />
+        <Route path={"/signin"} element={<Signin />} />
+        <Route path={"/login"} element={<Login />} />
       </Routes>
     </div>
   );
