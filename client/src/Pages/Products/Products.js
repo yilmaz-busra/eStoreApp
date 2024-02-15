b0c42a2c7609d943dd6a1d117191099b26a28141
busra <b.busraayilmaz@gmail.com>
2024-02-15 16:04:08 +0300

    imported-Card

---
+++ client/src/Pages/Products/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cards from "../../Components/Card/Card";
+
+function Products() {
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
+}
+
+export default Products;
