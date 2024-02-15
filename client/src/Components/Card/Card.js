2c8d19dc0d6bbaca8ba9c29aec741cc2e1bfa282
busra <b.busraayilmaz@gmail.com>
2024-02-15 23:58:40 +0300

    Getting Data & Card Component Imple.

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -11,26 +11,24 @@ import {
   Button,
   ButtonGroup,
 } from "@chakra-ui/react";
+import moment from "moment";
 
-function Cards() {
+function Cards({ item }) {
   return (
     <div>
       <Card maxW="sm">
         <CardBody>
           <Image
-            src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            src={item.photos[0]}
             alt="Green double couch with wooden legs"
             borderRadius="lg"
           />
           <Stack mt="6" spacing="3">
-            <Heading size="md">Living room Sofa</Heading>
-            <Text>
-              This sofa is perfect for modern tropical spaces, baroque inspired
-              spaces, earthy toned spaces and for people who love a chic design
-              with a sprinkle of vintage design.
-            </Text>
+            <Heading size="md">{item.title}</Heading>
+            <Text>{moment(item.createdAt).format("DD/MM/YYYY")}</Text>
+            <Text>{item.description.slice(0, 150)}...</Text>
             <Text color="blue.600" fontSize="2xl">
-              $450
+              {item.price}TL
             </Text>
           </Stack>
         </CardBody>
--- client/src/Pages/Products/Products.js
+++ client/src/Pages/Products/Products.js
@@ -2,18 +2,39 @@ import React from "react";
 
 import Cards from "../../Components/Card/Card";
 
-import { Grid } from "@chakra-ui/react";
+import { Grid, Center } from "@chakra-ui/react";
+import { useQuery } from "react-query";
+
+import { fetchProductList } from "../../api";
 
 function Products() {
+  const { error, data, isLoading } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: fetchProductList,
+  });
+
+  if (isLoading) return "Loading...";
+  if (error) return "An error has occurred: " + error.message;
+  console.log(data);
+
   return (
-    <div>
-      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-      </Grid>
-    </div>
+    <Center>
+      <div style={{ padding: "20px" }}>
+        <Grid
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(3, 1fr)",
+            xl: "repeat(4, 1fr)",
+          }}
+          gap={4}
+        >
+          {data.map((item, key) => {
+            return <Cards item={item} key={key} />;
+          })}
+        </Grid>
+      </div>
+    </Center>
   );
 }
 
