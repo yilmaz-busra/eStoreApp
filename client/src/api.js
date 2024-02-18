3c00cb3d77df092e330d03293144cd24c1ad3108
busra <b.busraayilmaz@gmail.com>
2024-02-18 03:11:56 +0300

    env endpoint change

--- client/src/api.js
+++ client/src/api.js
@@ -2,10 +2,23 @@ import axios from "axios";
 
 export const fetchProductList = async () => {
   try {
-    const { data } = await axios.get("http://localhost:4000/product");
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_BASE_ENDPOINT}/product`
+    );
     return data;
   } catch (error) {
     console.log("Hata");
     alert("HATAAAA");
+  }
+};
+
+export const fetchProductDetail = async (id) => {
+  try {
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_BASE_ENDPOINT}/product/${id}`
+    );
+    return data;
+  } catch (error) {
+    console.log("fetchProductDetail", error);
   }
 };
