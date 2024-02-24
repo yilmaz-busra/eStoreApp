0446f65251e3ef741c0e5cdfa35e288ece75cdcb
busra <b.busraayilmaz@gmail.com>
2024-02-25 01:25:16 +0300

    Fetchme & axios.interceptors added

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,24 @@
 import axios from "axios";
 
+axios.interceptors.request.use(
+  function (config) {
+    const { origin } = new URL(config.url);
+    const allowedOrigin = [process.env.REACT_APP_BASE_ENDPOINT];
+
+    const token = localStorage.getItem("access-token");
+
+    if (allowedOrigin.includes(origin)) {
+      config.headers.authorization = token;
+    }
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
 export const fetchProductList = async ({ pageParam = 0 }) => {
   try {
     const { data } = await axios.get(
@@ -27,6 +46,13 @@ export const fetchRegister = async (input) => {
   const { data } = await axios.post(
     `${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`,
     input
+  );
+  return data;
+};
+
+export const FetchMe = async () => {
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/me`
   );
   return data;
 };
