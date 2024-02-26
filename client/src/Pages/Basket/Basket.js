3400d82f4dc9c20b65a9d2635e9183fbb66604e7
busra <b.busraayilmaz@gmail.com>
2024-02-26 18:19:03 +0300

    TotalPrice & Navigate to ProductDetail &  EmptyBasket rendered

--- client/src/Pages/Basket/Basket.js
+++ client/src/Pages/Basket/Basket.js
@@ -9,19 +9,41 @@ import {
   Text,
   Button,
   Center,
+  Box,
 } from "@chakra-ui/react";
 import { useBasket } from "../../context/BasketContext";
+import { useNavigate } from "react-router-dom";
+import { SlBasketLoaded } from "react-icons/sl";
 
 function Basket() {
-  const { items } = useBasket();
+  const { items, removeFromBasket } = useBasket();
+
+  const navigate = useNavigate();
+
+  const sum = items.reduce((acc, obj) => acc + obj.price, 0);
+
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <Heading fontWeight="500" mt="10px">
-        Products In Basket
+        YOUR BASKET
       </Heading>
       <Center mt="25px">
+        {items.length < 1 && (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <SlBasketLoaded fontSize="400px" color="black" />
+            <Text fontWeight="500" fontSize="32px" mt="35px">
+              EMPTY
+            </Text>
+          </div>
+        )}
         {items.length > 0 && (
           <ul
             style={{
@@ -49,11 +71,17 @@ function Basket() {
                       maxW={{ base: "100%", sm: "200px" }}
                       src={item.photos[0]}
                       alt="photo"
+                      onClick={() => navigate(`/product/${item._id}`)}
                     />
 
                     <Stack>
                       <CardBody>
-                        <Heading size="md">{item.title}</Heading>
+                        <Heading
+                          size="md"
+                          onClick={() => navigate(`/product/${item._id}`)}
+                        >
+                          {item.title}
+                        </Heading>
                         <Text mt="2">{item.description.slice(0, 75)}...</Text>
                       </CardBody>
 
@@ -61,8 +89,12 @@ function Basket() {
                         <Text py="2" fontWeight="600">
                           {item.price}$
                         </Text>
-                        <Button variant="solid" colorScheme="blue">
-                          Buy Latte
+                        <Button
+                          variant="solid"
+                          colorScheme="red"
+                          onClick={() => removeFromBasket(item._id)}
+                        >
+                          Remove from basket
                         </Button>
                       </CardFooter>
                     </Stack>
@@ -70,6 +102,17 @@ function Basket() {
                 </li>
               );
             })}
+            <li>
+              <Box
+                borderRadius="md"
+                color="black"
+                justifyContent="space-between"
+                display="flex"
+              >
+                <Text fontWeight="500">Total:{sum}$</Text>{" "}
+                <Button>Go to the Payment</Button>
+              </Box>
+            </li>
           </ul>
         )}
       </Center>
