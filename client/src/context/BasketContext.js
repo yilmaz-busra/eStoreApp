d381c220efa12804e33e659e52bc2e505d53f3b2
busra <b.busraayilmaz@gmail.com>
2024-02-26 18:20:25 +0300

    removeFromBasket imple & localStorge imple

--- client/src/context/BasketContext.js
+++ client/src/context/BasketContext.js
@@ -1,21 +1,35 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BasketContext = createContext();
 
 const BasketProvider = ({ children }) => {
-  const [items, setItems] = useState([]);
+  const defaultBasket = JSON.parse(localStorage.getItem("basket")) || [];
+
+  const [items, setItems] = useState(defaultBasket);
+
+  useEffect(() => {
+    localStorage.setItem("basket", JSON.stringify(items));
+  }, [items]);
 
   const addtoBasket = (item, findBasketItem) => {
     if (!findBasketItem) {
       return setItems((prev) => [...prev, item]);
     }
+
     const filtered = items.filter((item) => item._id !== findBasketItem._id);
     setItems(filtered);
   };
+
+  const removeFromBasket = (item_id) => {
+    const filtered = items.filter((item) => item._id !== item_id);
+    setItems(filtered);
+  };
+
   const values = {
     addtoBasket,
     items,
     setItems,
+    removeFromBasket,
   };
   return (
     <BasketContext.Provider value={values}>{children}</BasketContext.Provider>
