92d7dbf422917386975046d03890c4e3e520c693
busra <b.busraayilmaz@gmail.com>
2024-02-29 23:47:35 +0300

    Admin Route & sub pages created & minor styled

---
+++ client/src/Pages/Admin/Admin.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+
+import Home from "./Home/Home";
+import Orders from "./Orders/Orders.js";
+import Products from "./Products/Products";
+import Error from "../Error404/error.js";
+import "../../Styles/admin.css";
+function Admin() {
+  return (
+    <div>
+      <nav>
+        <ul className="admin-menu">
+          <li>
+            <Link to={"/admin"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/admin/products"}>Products</Link>
+          </li>
+          <li>
+            <Link to={"/admin/orders"}>Orders</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"orders"} element={<Orders />} />
+        <Route path={"products"} element={<Products />} />
+        <Route path={"*"} element={<Error />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default Admin;
