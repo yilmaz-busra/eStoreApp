95c65e36d36d43750213275b6e377b427907e923
busra <b.busraayilmaz@gmail.com>
2024-03-02 23:22:49 +0300

    admin order page created & orders listed

--- client/src/Pages/Admin/Orders/Orders.js
+++ client/src/Pages/Admin/Orders/Orders.js
@@ -1,7 +1,62 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { fetchOrder } from "../../../api";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  TableCaption,
+  TableContainer,
+  Text,
+} from "@chakra-ui/react";
 
 function Orders() {
-  return <div>Orders</div>;
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["admin:orders"],
+    queryFn: fetchOrder,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  console.log(data);
+
+  return (
+    <div>
+      <Text fontSize="2xl">Orders</Text>
+
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>Table of orders</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>User</Th>
+              <Th>Address</Th>
+              <Th isNumeric>Items</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((order) => {
+              return (
+                <Tr key={order?._id}>
+                  <Th>{order?.user?.email}</Th>
+                  <Th>{order?.adress}</Th>
+                  <Th isNumeric>{order?.items?.length}</Th>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
 export default Orders;
